e37f8324a8abd95a8008a90e0d43cb8fdb22ffec
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-10-06 11:21:32 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+const PORT = 8080
+
+const app = express()
+app.use(express.json())
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!")
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is running on port ${process.env.PORT}`)
+})
